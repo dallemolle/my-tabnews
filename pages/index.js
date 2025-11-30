206dbdbb3ea350aa9d249620903e27baed14f10e
dallemolle <leandrodm83@gmail.com>
2025-11-30 01:57:07 +0000

    pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Iniciando novo projeto, estarei usando esse conhecimento para criar um sistema que com sua ajuda, poderá contribuir com sua produtividade!</h1>
+}
+
+export default Home;
