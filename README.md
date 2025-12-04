33e4b36b20f8f1ae87ef19214daa0c24f73cec36
dallemolle <leandrodm83@gmail.com>
2025-12-04 19:57:35 +0000

    adicionado scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my-tabnews
+
 Implementação para desenvolvimento de conhecimento usando como base https://tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Iniciando novo projeto, estarei usando esse conhecimento para criar um sistema que com sua ajuda, poderá contribuir com sua produtividade!S2</h1>
+function Home() {
+  return (
+    <h1>
+      Iniciando novo projeto, estarei usando esse conhecimento para criar um
+      sistema que com sua ajuda, poderá contribuir com sua produtividade!S2
+    </h1>
+  );
 }
 
 export default Home;
