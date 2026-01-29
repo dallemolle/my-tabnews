6cfc0e1ed318bf0c252c0d74387187dafb12a0a3
dallemolle <leandrodm83@gmail.com>
2026-01-29 20:22:01 +0000

    add `/api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Iniciando novo projeto, estarei usando esse conhecimento para criar um
-      sistema que com sua ajuda, poderá contribuir com sua produtividade!S2
+      sistema que com sua ajuda, poderá contribuir com sua produtividade!S2!
     </h1>
   );
 }
